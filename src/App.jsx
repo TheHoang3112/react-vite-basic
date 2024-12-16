c32514ebe58bc3ea56960b04857c71a8c26d4c3d
Hoàng <thehoang3112u@gmail.com>
2024-12-16 21:37:07 +0700

    change code follow video 34 add props into file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import reactLogo from "./assets/react.svg"
 import "./components/Todo/todo.css"
 
 const App = () => {
+  const name = "HoangCT";
+  const age = 26;
+  const data = {
+    address: "HCM",
+    country: "VietNam"
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData 
+        name={name}
+        age={age}
+        data={data}
+      />
       <div className="todo-image">
         <img src={reactLogo} className="logo"/>
       </div>
--- src/components/Todo/TodoData.jsx
+++ src/components/Todo/TodoData.jsx
@@ -1,8 +1,12 @@
 
 
-const TodoData = () => {
+const TodoData = (props) => {
+    const {name,age,data} = props;
     return (
         <div className="todo-data">
+            <div>My name is {name}</div>
+            <div>Im {age}</div>
+            <div>I Live {data.address} and {data.country}</div>
             <div> Learning React </div>
             <div> Watching Youtube </div>
         </div>
