1c8973d413ce004b310865cadb134354357460b1
Hoàng <thehoang3112u@gmail.com>
2024-12-16 21:53:34 +0700

    change code follow video 35 add function props

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,16 @@ const App = () => {
     address: "HCM",
     country: "VietNam"
   }
+
+  const addNewTodo = (name) => {
+    alert(`Call function name ${name}`)
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew />
+      <TodoNew 
+        addNewTodo={addNewTodo}
+      />
       <TodoData 
         name={name}
         age={age}
--- src/components/Todo/TodoNew.jsx
+++ src/components/Todo/TodoNew.jsx
@@ -1,6 +1,9 @@
 
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    const {addNewTodo} = props;
+
+    addNewTodo("HoangCT");
     return (
         <div className="todo-new">
             <input type="text" />
