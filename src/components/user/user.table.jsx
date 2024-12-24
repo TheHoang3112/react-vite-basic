5851ec563acba53b63fd2cbf392dbe9d7e3d8393
Hoàng <thehoang3112u@gmail.com>
2024-12-24 15:48:55 +0700

    change code follow video 74 Design Modal Popup create user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,43 +1,34 @@
-import {  Table } from 'antd';
-import { fetchAllUserAPI } from '../../services/api.service';
-import { useEffect, useState } from 'react';
+import { Table } from "antd";
+import { fetchAllUserAPI } from "../../services/api.service";
+import { useEffect, useState } from "react";
 
 const UserTable = () => {
-    const [dataUsers, setDataUsers] = useState([
-        { _id: "eric", fullName: 25, email: "hn" },
-        { _id: "hoidanit", fullName: 25, email: "hcm" }
-    ]);
-    useEffect(() => {
-        loadUser();
-    },[]);
-    const columns = [
-        {
-            title: 'Id',
-            dataIndex: '_id'
-        },
-        {
-            title: 'Full Name',
-            dataIndex: 'fullName'
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email'
-        }
-    ];
-    const loadUser = async () => {
-        const res = await fetchAllUserAPI();
-        setDataUsers(res.data);
-    }
+  const [dataUsers, setDataUsers] = useState([]);
+  useEffect(() => {
+    loadUser();
+  }, []);
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+  ];
+  const loadUser = async () => {
+    const res = await fetchAllUserAPI();
+    setDataUsers(res.data);
+  };
 
-    // loadUser();
-    // console.log(">>Run render");
-    return (
-        <Table 
-        columns={columns} 
-        dataSource={dataUsers} 
-        rowKey={"_id"}
-        />  
-    );
-}
+  // loadUser();
+  // console.log(">>Run render");
+  return <Table columns={columns} dataSource={dataUsers} rowKey={"_id"} />;
+};
 
 export default UserTable;
