8d5573e75ebe773d6b29f456822050045c7ea586
Hoàng <thehoang3112u@gmail.com>
2024-12-24 08:06:21 +0700

    change code follow video 69 add methor post axios

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,63 +1,65 @@
 import { Button, Input } from "antd";
 import { useState } from "react";
+import axios from "axios";
 
 const UserForm = () => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [passWord, setPassWord] = useState("");
-  const [phone, setPhone] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
 
-  const handleClickBtn = () => {
-    console.log(">>>Check input: ", { fullName, email, passWord, phone });
-  };
-  return (
-    <div className="user-form" style={{ margin: "20px 0" }}>
-      <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
-        <div>
-          <span>Full Name</span>
-          <Input
-            value={fullName}
-            onChange={(event) => {
-              setFullName(event.target.value);
-            }}
-          />
+    const handleClickBtn = () => {
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data)
+        console.log(">>> check state: ", { fullName, email, password, phone })
+    }
+
+    return (
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <div>
+                    <span>Full Name</span>
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Phone number</span>
+                    <Input
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <Button
+                        // onClick={() => handleClickBtn()}
+                        onClick={handleClickBtn}
+                        type="primary"> Create User </Button>
+                </div>
+            </div>
         </div>
-        <div>
-          <span>Email</span>
-          <Input
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <span>Password</span>
-          <Input.Password
-            value={passWord}
-            onChange={(event) => {
-              setPassWord(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <span>Phone number</span>
-          <Input
-            value={phone}
-            onChange={(event) => {
-              setPhone(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <Button onClick={handleClickBtn} type="primary">
-            {" "}
-            Create User{" "}
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default UserForm;
