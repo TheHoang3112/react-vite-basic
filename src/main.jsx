473dc6ebbc1caca10e3c5637cd18a0a8c8e0636d
Hoàng <thehoang3112u@gmail.com>
2024-12-27 22:34:55 +0700

    change code follow video 81 Setup Eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./styles/global.css"
-import TodoApp from './components/Todo/TodoApp.jsx';
-import ErrorPage from './pages/error.jsx';
-import UserPage from './pages/user.jsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/global.css";
+import TodoApp from "./components/Todo/TodoApp.jsx";
+import ErrorPage from "./pages/error.jsx";
+import UserPage from "./pages/user.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <TodoApp />
+        element: <TodoApp />,
       },
       {
         path: "/users",
-        element: <UserPage />
+        element: <UserPage />,
       },
       {
         path: "/books",
-        element: <div>BookPage</div>
-      }
-    ]
+        element: <div>BookPage</div>,
+      },
+    ],
   },
   {
     path: "/login",
-    element: <div>login page</div>
+    element: <div>login page</div>,
   },
   {
     path: "/register",
-    element: <div>register page</div>
-  }
+    element: <div>register page</div>,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
   //</React.StrictMode>,
-)
+);
