c03abbe16e69d7261c744328452a0add6f3337e5
Hoàng <thehoang3112u@gmail.com>
2024-12-28 07:57:14 +0700

    change code follow video 82 show avatar upload

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -19,6 +19,34 @@ const ViewUserDetail = (props) => {
           <br />
           <p>Phone number: {dataDetail.phone}</p>
           <br />
+          <p>Avatar: </p>
+          <br />
+          <div>
+            <img
+              height={100}
+              width={150}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                dataDetail.avatar
+              }`}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="btnUpload"
+              style={{
+                display: "block",
+                width: "fit-content",
+                marginTop: "15px",
+                padding: "5px 10px",
+                background: "orange",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Upload Avatar
+            </label>
+            <input type="file" hidden id="btnUpload" />
+          </div>
         </>
       ) : (
         <>Không có dữ liệu</>
